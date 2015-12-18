f66035dc73683b157a74be4db6989826244e30f9
Eric <ericwfeinstein@gmail.com>
2015-12-18 18:15:23 -0500

    Fixed Green/Red colors

--- heatmap-three-measure-cell.js
+++ heatmap-three-measure-cell.js
@@ -1,7 +1,7 @@
 (function() {
 looker.plugins.visualizations.add({
-  id: 'heatmap',
-  label: 'Heatmap',
+  id: 'heatmapjs1',
+  label: 'Heatmapjs1',
   options: {
     
     colorPreSet:
@@ -12,9 +12,9 @@ looker.plugins.visualizations.add({
       section: 'Data',
       values: [{'Custom': 'c'},
       {'Tomato to Steel Blue': '#F16358,#DF645F,#CD6566,#BB666D,#A96774,#97687B,#856982,#736A89,#616B90,#4F6C97,#3D6D9E'},
-      {'Pink to Black': '#170108, #300211, #49031A, #620423, #79052B, #910734, #AA083D, #C30946, #DA0A4E, #F30B57, #F52368, #F63378, #F63C79, #F75389, #F86C9A, #F985AB, #FB9DBC, #FCB4CC, #FDCDDD, #FEE6EE'},
-      {'Green to Red': '#7FCDAE, #7ED09C, #7DD389, #85D67C, #9AD97B, #B1DB7A, #CADF79, #E2DF78, #E5C877, #E7AF75, #EB9474, #EE7772'},
-      {'White to Green': '#ffffe5,#f7fcb9 ,#d9f0a3,#addd8e,#78c679,#41ab5d,#238443,#006837,#004529'},
+      {'Pink to Black': '#170108,#300211,#49031A,#620423,#79052B,#910734,#AA083D, #C30946,#DA0A4E,#F30B57,#F52368,#F63378,#F63C79,#F75389,#F86C9A,#F985AB,#FB9DBC,#FCB4CC,#FDCDDD,#FEE6EE'},
+      {'Green to  Red': '#ff4000,#ff8000,#ffbf00,#ffff00,#bfff00,#80ff00,#40ff00,#00ff00'},
+      {'White to Green': '#ffffe5,#f7fcb9,#d9f0a3,#addd8e,#78c679,#41ab5d,#238443,#006837,#004529'},
       {'Sunset': '#ffffcc,#ffeda0,#fed976,#feb24c,#fd8d3c,#fc4e2a,#e31a1c,#b10026'}],
        default: 'c',
       order: 1
@@ -416,6 +416,8 @@ looker.plugins.visualizations.add({
         }
       });
 
+    // console.log(resp);
+
     var tbody = table.selectAll('tbody')
       .data([data]);
 
@@ -453,11 +455,13 @@ looker.plugins.visualizations.add({
 
     tds.style('background-color', function(d) {
         if ((d.type == 'measure' || d.type == 'table_calculations') && d.data.rendered !== '') {
-            // console.log(d);
+            console.log(d);
 
             if(settings.colorFirstColumn == false && d.column == 0){ // check first column setting, leave blank if not enabled
                 return '#f6f8fa';
-            }else if(settings.colorMeasure == '1'){
+            }else if(d.is_total){
+                return '#f6f8fa';
+             }else if(settings.colorMeasure == '1'){
                 if ((!(colorMinMaxRange[0]==null) && d.data.value < Number(colorMinMaxRange[0]) ) || ( !(colorMinMaxRange[1]==null) && d.data.value > Number(colorMinMaxRange[1]))){
                   return '#f6f8fa';
                 }
