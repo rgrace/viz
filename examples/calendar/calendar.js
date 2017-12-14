d68dbadfd92d30fd07962793eb85e8ebcf85f85c
Jamie Davidson <jdavidson@gmail.com>
2017-12-14 09:58:24 -0800

    Calendar input (#61)
    * cleaning up
    
    * fix handle errors
    
    * clean checks

--- examples/calendar/calendar.js
+++ examples/calendar/calendar.js
@@ -26,36 +26,52 @@ looker.plugins.visualizations.add({
 
     // [{x.name: value, y.name: value}, ]
     let series = []
-    data.forEach(function(datum) {
+    data.filter(function(d) {
+      return d3v4.timeParse("%Y-%m-%d")(d[x.name]["value"])
+    }).forEach(function(d) {
       let point = {}
-      point[x.name] = datum[x.name]["value"]
-      point[y.name] = datum[y.name]["value"]
+      point[x.name] = d[x.name]["value"]
+      point[y.name] = d[y.name]["value"]
       series.push(point)
     })
 
     // {date: value, }
     let formattedData = d3v4.nest()
-      .key(function(d) { return d[x.name]; })
-      .rollup(function(d) { return d[0][y.name]; })
-      .map(series);
+      .key(function(d) { return d[x.name] })
+      .rollup(function(d) { return d[0][y.name] })
+      .map(series)
 
-    let formatter = formatType(y.value_format);
+    let formatter = formatType(y.value_format)
 
     return {
       data: formattedData,
       formatter: formatter,
     }
   },
-
+  handleErrors(queryResponse) {
+    if (!(queryResponse.fields.dimensions[0].is_timeframe &&
+          queryResponse.fields.dimensions[0].time_interval.name === "day")) {
+      this.addError({
+        group: "date-req",
+        title: "Incompatible Data",
+        message: "Calendar Visualization Requires a Date Dimension",
+      })
+      return false
+    } else {
+      this.clearErrors("date-req")
+    }
+    return true
+  },
   // Render in response to the data or settings changing
   update: function(data, element, config, queryResponse) {
     if (!handleErrors(this, queryResponse, {
       min_pivots: 0, max_pivots: 0,
       min_dimensions: 1, max_dimensions: 1,
       min_measures: 1, max_measures: 1,
-    })) return;
-    this.create(element, config);
-    let formattedData = this.prepare(data, queryResponse);
-    return calendarView(element, formattedData, config.color_range);
+    })) return
+    if (!this.handleErrors(queryResponse)) return
+    this.create(element, config)
+    let formattedData = this.prepare(data, queryResponse)
+    return calendarView(element, formattedData, config.color_range)
   }
-});
+})
